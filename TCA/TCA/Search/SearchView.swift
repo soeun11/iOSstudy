d036329fe85abc95a8343bb21881fbcdf5fd508b
soonny <134983918+soeun11@users.noreply.github.com>
2026-02-12 23:51:27 +0900

    [feat] 키워드 검색 목록 구현 + X 버튼도 구현

--- TCA/TCA/Search/SearchView.swift
+++ TCA/TCA/Search/SearchView.swift
@@ -13,10 +13,21 @@ import UIKit
 struct SearchView: View {
     @Bindable var store:  StoreOf<SearchReducer>
     @Environment(\.modelContext) private var context
-    @Query private var keyword: [Keyword]
+    @Query(sort: \Keyword.date, order: .reverse) private var keyword: [Keyword]
     @FocusState private var isFocused: Bool
     
     var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack {
+                    textField
+                    contentView
+                }
+            }
+        }
+    }
+    
+    private var textField: some View {
         TextField("키워드를 검색해보세요", text: $store.keyword.sending(\.inputText))
             .frame(height: 40)
             .font(.system(size: 15))
@@ -24,29 +35,67 @@ struct SearchView: View {
             .padding(.leading, 12)
             .padding(.vertical, 6)
             .focused($isFocused)
-            .overlay(alignment: .topTrailing) {
+            .overlay(alignment: .trailing) {
                 Button {
                     store.send(.clearText)
                 } label: {
                     Image(systemName: "xmark.circle.fill")
-                        .foregroundStyle(Color.gray)
+                        .foregroundStyle(.gray)
                 }
+                .padding(.trailing, 12)
             }
             .overlay {
                 RoundedRectangle(cornerRadius: 8)
                     .stroke(Color.gray, lineWidth: 1)
             }
             .onSubmit {
-                //검색
                 //키워드 저장
                 saveKeyword(keyword: store.keyword)
                 print("keyword\(keyword.first?.title)")
+                //검색
             }
+            .padding(.horizontal, 20)
+    }
+    
+    private var contentView: some View {
+        ForEach(keyword, id: \.self) {keyword in
+            HStack{
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundStyle(.black)
+                    Text(keyword.title)
+                        .font(.system(size: 16))
+                        .padding(.leading, 10)
+                        .lineLimit(1)
+                    Spacer()
+                }
+                .onTapGesture {
+                    //TODO: 검색
+                }
+                Button {
+                    deleteKeyword(keyword: keyword)
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundStyle(.black)
+                }
+            }
+            .padding(20)
+        }
     }
     
     func saveKeyword(keyword: String) {
         let data = Keyword(title: keyword, date: Date.now)
         context.insert(data)
+        try? context.save()
+    }
+    
+    func deleteKeyword(keyword: Keyword) {
+        //TODO: 밑에 코드 이해하기 - 키워드 탐색을 위해 존재
+//        let descriptor =  FetchDescriptor<Keyword>(predicate: #Predicate{$0.title == keyword})
+//        if let model = try? context.fetch(descriptor).first {
+//            context.delete(model)
+//        }
+        context.delete(keyword)
         try? context.save()
     }
 }
