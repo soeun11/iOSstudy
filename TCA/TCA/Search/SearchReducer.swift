314afee397753db0667c61417f3292b76d4d15ad
soonny <134983918+soeun11@users.noreply.github.com>
2026-02-12 15:58:51 +0900

    [feat] TCA Reducer 기본세팅

---
+++ TCA/TCA/Search/SearchReducer.swift
@@ -0,0 +1,22 @@
+//
+//  SearchReducer.swift
+//  TCA
+//
+//  Created by 소은 on 2/12/26.
+//
+
+import ComposableArchitecture
+
+@Reducer
+struct SearchReducer {
+    @ObservableState
+    struct State {
+        
+    }
+    
+    @Action
+    enum Action {
+        
+    }
+}
+
--- TCA/TCA/Search/Untitled.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Untitled.swift
-//  TCA
-//
-//  Created by 소은 on 2/12/26.
-//
-
